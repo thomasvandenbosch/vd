9264ca208cf832ccf7c06b5f724a14d9e7f90535
Thomas Van den Bosch <thomasvandenbosch@outlook.be>
2018-12-09 11:22:53 +0100

    Initial project state

---
+++ VrijeDagen/Form1.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtResultaat = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtResultaat
+        '
+        Me.txtResultaat.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResultaat.Location = New System.Drawing.Point(13, 13)
+        Me.txtResultaat.Multiline = True
+        Me.txtResultaat.Name = "txtResultaat"
+        Me.txtResultaat.ReadOnly = True
+        Me.txtResultaat.Size = New System.Drawing.Size(212, 236)
+        Me.txtResultaat.TabIndex = 1
+        Me.txtResultaat.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtResultaat)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Komende vrije dagen"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtResultaat As TextBox
+End Class
---
+++ VrijeDagen/Form1.vb
@@ -0,0 +1,79 @@
+﻿'*****************************************************************************************************************
+'*Window met 1 textbox. Geeft bij het opstarten een lijst van de komende 15 vrije dagen (met circulerend regime).*
+'*****************************************************************************************************************
+
+'TODO Weeknummers tonen bij overzicht
+
+Imports System.IO
+
+Public Class Form1
+
+    Friend Const txtPath = "vd.txt"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        MinimizeBox = False
+        MaximizeBox = False
+
+        Dim data As String()
+
+
+        If File.Exists(txtPath) Then
+            Using fileReader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(txtPath)
+                Dim stringReader As String = fileReader.ReadLine()
+                data = stringReader.Split(" ")
+            End Using
+        Else
+            data = {"19/11/2018", "V"}  'Standaardwaarden gebruiken als bestand niet bestaat
+        End If
+
+
+        Dim datum As Date = CType(data(0), Date)
+        Dim ploeg As String = data(1)
+
+        Do While datum < LaatsteMaandag()
+            datum = VolgendeDatum(datum)
+            ploeg = NieuwePloeg(ploeg)
+        Loop
+
+        File.WriteAllText(txtPath, $"{datum.Day.ToString().PadLeft(2, "0")}/{datum.Month.ToString().PadLeft(2, "0")}/{datum.Year} {ploeg}")
+
+        txtResultaat.Text += $"{datum.ToString("ddd dd/MM/yyyy")} {ploeg}" & vbCrLf
+        txtResultaat.Text += New String("-", 15) & vbCrLf
+
+        For i = 1 To 14
+            datum = VolgendeDatum(datum)
+            ploeg = NieuwePloeg(ploeg)
+            txtResultaat.Text += $"{datum.ToString("ddd dd/MM/yyyy")} {ploeg}" & vbCrLf
+        Next i
+        txtResultaat.Text = txtResultaat.Text.Substring(0, txtResultaat.TextLength - 2)
+    End Sub
+
+    Private Function VolgendeDatum(ByVal datum As Date) As Date
+        If datum.DayOfWeek = DayOfWeek.Friday Then
+            Return datum.AddDays(3)
+        Else
+            Return datum.AddDays(8)
+        End If
+    End Function
+
+    Private Function NieuwePloeg(ByVal ploeg As String) As String
+        If ploeg = "V" Then
+            Return "L"
+        Else
+            Return "V"
+        End If
+    End Function
+
+    Private Function LaatsteMaandag() As Date
+        Dim vandaag As Date = Date.Today
+        Dim weekdag As Integer = vandaag.DayOfWeek
+
+        If weekdag = 1 Then 'vandaag is maandag
+            Return vandaag
+        ElseIf weekdag = 0 Then 'vandaag is zondag
+            Return vandaag.AddDays(-6)
+        Else
+            Return vandaag.AddDays(1 - weekdag)
+        End If
+    End Function
+End Class
